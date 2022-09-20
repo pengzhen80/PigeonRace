80784582a94fa8e81aeb7e19d9bf9e66e2cc37ca
pengzhen <guopengzhen2015@icloud.com>
2022-09-20 21:36:09 +0800

    finish api

--- Django_learning/dataAnalysisPlatform/routeAnalysisPlatform/dataAnalysisManagement/apiManagement.py
+++ Django_learning/dataAnalysisPlatform/routeAnalysisPlatform/dataAnalysisManagement/apiManagement.py
@@ -1,5 +1,6 @@
 from asyncio import shield
 from cgi import test
+from operator import mod
 from re import T
 from unittest import result
 from requests import delete
@@ -9,6 +10,7 @@ from collections import defaultdict
 from datetime import datetime
 import hashlib
 import copy
+import csv
 
 
 class DBManagement ():
@@ -67,25 +69,27 @@ class DBManagement ():
         for activity in self.activities:
             self.api_getActivitity(activity['activity_id'], self.mxid, activity['activity_name'],
                                    activity['release_date'], activity['release_time'])
+            self.api_readModule(activity['activity_id'])
 
         return res
 
-    def api_readTrainRecord(self,activity_id):
+    def api_readTrainRecord(self, activity_id, module_id):
         url = 'http://skyleader3.yuansan.com/api/SkyLeader/readTrainRecord'
 
-        myobj = {"trainrecordid": "string",
-                 "mxid": self.mxid,
-                 "moduleid": "string",
-                 "doveid": "string",
-                 "activity_id": activity_id,
-                 "recordname": "string"
-                 }
+        myobj = {
+            "mxid": self.mxid,
+            "moduleid": module_id,
+            "activity_id": activity_id,
+        }
 
         x = requests.post(url, data=myobj)
         res = x.json()
         # print(res,type(res))
         res = json.loads(res)
-        print(res)
+        for trainRecord in res:
+            print(trainRecord['trainrecordid'])
+            self.api_readCloudLocusText(
+                activity_id, module_id, trainRecord['trainrecordid'])
         return res
 
     def api_getActivitity(self, activity_id, mxid, activity_name, release_date, release_time):
@@ -102,11 +106,66 @@ class DBManagement ():
         x = requests.post(url, data=myobj)
         res = x.json()
         res = json.loads(res)
-        print('activity:',res)
+        print('activity:', res)
         # self.activitys.append(res)
         # self.activitys += self.activitys+res
         # print(self.activitys)
         return res
+
+    def api_readCloudLocusText(self, activity_id, module_id, trainRecord_id):
+        url = 'http://skyleader3.yuansan.com/api/SkyLeader/readCloudLocusText'
+
+        myobj = {
+            "moduleid": module_id,
+            "trainrecordid": trainRecord_id,
+            "activity_id": activity_id
+        }
+
+        x = requests.post(url, data=myobj)
+        res = x.json()
+        res = json.loads(res)
+        print('routes', res)
+        # self.activitys.append(res)
+        # self.activitys += self.activitys+res
+        # print(self.activitys)
+        return res
+
+    def api_readModule(self, activity_id):
+        dataList = []
+        with open('csvfiles/gpsId_ringId.csv', newline='') as csvfile:
+            spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+
+            for row in spamreader:
+                cell = {}
+                # print(', '.join(row))
+                # print(type(row),len(row))
+                rowList = row[0].split(',')
+                cell['gpsId'] = rowList[0]
+                cell['ringId'] = rowList[1]
+                # print(cell)
+                dataList.append(cell)
+
+        for data in dataList:
+            print(data)
+            url = 'http://skyleader3.yuansan.com/api/SkyLeader/readCloudModule'
+
+            myobj = {
+                "activity_id": activity_id,
+                "mxid": self.mxid,
+                "ringid": data['ringId'],
+                "laserid": data['gpsId']
+            }
+
+            x = requests.post(url, data=myobj)
+            res = x.json()
+            res = json.loads(res)
+            # print('module:', res)
+            for module in res:
+                # print('module_id',module['modlueid'])
+                # print(module)
+                print('module_id:', module['moduleid'])
+                self.api_readTrainRecord(activity_id, module['moduleid'])
+        return
 
     def getActivities(self):
         return self.activities
