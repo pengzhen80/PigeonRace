f7a15c42f0e91769a44ec0f0cb94d9b90c971243
pengzhen <guopengzhen2015@icloud.com>
2022-10-27 12:18:48 +0800

    update

--- Django_learning/dataAnalysisPlatform/routeAnalysisPlatform/routeAnalysisPlatform/settings.py
+++ Django_learning/dataAnalysisPlatform/routeAnalysisPlatform/routeAnalysisPlatform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1&w69-@_4l$ucpmc2#0o3j)ily#jqq9hdxrq@v+j7w$)p$sj4j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
